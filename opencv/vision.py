fb45aaf18baf82c5689a960bd847cf8ab994d4e4
Gregory Kelly <git@gregk.ca>
2021-03-16 01:53:58 -0400

    Added more filters to found rectangles

--- opencv/vision.py
+++ opencv/vision.py
@@ -14,7 +14,8 @@ done = False
 
 ratioUpper = 2.5
 ratioLower = 0.5
-
+T = 0
+B = 100
 
 def onThresh(val):
     global blocksize
@@ -33,6 +34,14 @@ def onRatioLower(val):
     global ratioLower
     ratioLower = val/10
 
+    
+def onTop(val):
+    global T
+    T = val
+def onBottom(val):
+    global B
+    B = val
+
 onThresh(720)
 onBlur(5)
 
@@ -46,15 +55,19 @@ def doOnce():
     cv2.createTrackbar('blur', 'blurred', 1, 50, onBlur)
     cv2.createTrackbar('ratioUpper', 'results', 1, 100, onRatioUpper)
     cv2.createTrackbar('ratioLower', 'results', 1, 100, onRatioLower)
+    cv2.createTrackbar('T', 'results', 0, 480, onTop)
+    cv2.createTrackbar('B', 'results', 0, 480, onBottom)
 
 
 while(True):
     # Load image
     ret, frame = cap.read()
-    raw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('image', raw)
+    raw = frame
 
-    img = cv2.adaptiveThreshold(raw, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('image', img)
+
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                 cv2.THRESH_BINARY, blocksize, 2)
     cv2.imshow('thresh', img)
 
@@ -73,8 +86,9 @@ while(True):
                                 cv2.CHAIN_APPROX_SIMPLE)
     for c in contours:
         x,y,w,h = cv2.boundingRect(c)
-        if w/h < ratioUpper and w/h > ratioLower:
+        if w/h < ratioUpper and w/h > ratioLower and y > T and y+h < B:
             cv2.rectangle(raw, (x,y), (x+w,y+h), (0, 0, 255), 2)
+    cv2.rectangle(raw, (0,T), (640, B), (0,255,0), 2)
     # type(contours[0])
     # cv2.drawContours(raw, contours, -1, (0,255,0), 3)
     # cv2.imshow('contours', img)
