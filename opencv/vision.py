58477350324c8fa2c8da0d4d3c527951374d6317
Gregory Kelly <git@gregk.ca>
2021-03-16 01:32:43 -0400

    Wrote openCV code to get some plates (and alot of false-positives)

---
+++ opencv/vision.py
@@ -0,0 +1,88 @@
+import numpy as np
+import imutils
+import cv2
+
+print(cv2.getVersionString())
+
+cap = cv2.VideoCapture(0)
+
+print(cap.isOpened())
+
+blocksize = 5
+blurRad = 5
+done = False
+
+ratioUpper = 2.5
+ratioLower = 0.5
+
+
+def onThresh(val):
+    global blocksize
+    blocksize = val + (val % 2-1)
+
+
+def onBlur(val):
+    global blurRad
+    blurRad = val + (val % 2-1)
+
+def onRatioUpper(val):
+    global ratioUpper
+    ratioUpper = val/10
+
+def onRatioLower(val):
+    global ratioLower
+    ratioLower = val/10
+
+onThresh(720)
+onBlur(5)
+
+
+def doOnce():
+    global done
+    if done:
+        return
+    done = True
+    cv2.createTrackbar('threshold', 'thresh', 720, 10000, onThresh)
+    cv2.createTrackbar('blur', 'blurred', 1, 50, onBlur)
+    cv2.createTrackbar('ratioUpper', 'results', 1, 100, onRatioUpper)
+    cv2.createTrackbar('ratioLower', 'results', 1, 100, onRatioLower)
+
+
+while(True):
+    # Load image
+    ret, frame = cap.read()
+    raw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('image', raw)
+
+    img = cv2.adaptiveThreshold(raw, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                cv2.THRESH_BINARY, blocksize, 2)
+    cv2.imshow('thresh', img)
+
+    img = cv2.medianBlur(img, blurRad)
+    cv2.imshow('blurred', img)
+
+    img = cv2.Canny(img, 1, 100)
+    cv2.imshow('canny', img)
+
+    # ret, thresh = cv2.threshold(img, 127, 255, 0)
+    # print(thresh[0])
+    # type(thresh[0])
+    # contours, hierarchy  = cv2.findContours(thresh, 1, 2)
+
+    contours, hierarchy = cv2.findContours(img.copy(), cv2.RETR_TREE,
+                                cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        x,y,w,h = cv2.boundingRect(c)
+        if w/h < ratioUpper and w/h > ratioLower:
+            cv2.rectangle(raw, (x,y), (x+w,y+h), (0, 0, 255), 2)
+    # type(contours[0])
+    # cv2.drawContours(raw, contours, -1, (0,255,0), 3)
+    # cv2.imshow('contours', img)
+
+    cv2.imshow('results', raw)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    doOnce()
+
+cap.release()
+cv2.destroyAllWindows()
