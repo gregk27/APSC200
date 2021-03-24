6889564626037910e6b143cf95ff4a5a344dc05c
Gregory Kelly <git@gregk.ca>
2021-03-23 21:33:10 -0400

    Lidar no longer detects ego vehicle

--- process.m
+++ process.m
@@ -1,7 +1,9 @@
 function [] = process(scenario, objects, ptCloud, world, vehicle)
-    persistent fig
+    persistent fig;
+    persistent detected;
     if isempty(fig)
         fig = figure;
+        detected = [];
     end
 
     % objectsStruct = [objects{:}];
@@ -15,15 +17,24 @@ function [] = process(scenario, objects, ptCloud, world, vehicle)
 
     % plot3(allPosInertial(1,:), allPosInertial(2,:), allPosInertial(3,:), 'b. ', 'Parent', hTopViewAxes);
     
-    % Cluster point clouds
+    % Filter out road surface
     [model1,inlierIndices,outlierIndices] = pcfitplane(ptCloud,1,[0,0,1],0.02);
     cloud = select(ptCloud,inlierIndices);
-    [labels, numClusters] = pcsegdist(cloud, 0.5);
     
+    % Eliminate points on vehicle roof
+    magnitudes = sqrt((cloud.Location(:, 1)-1).^2+cloud.Location(:, 2).^2);
+    cloud = pointCloud(cloud.Location(magnitudes > 1, :));
+    
+    % Cluster to get cuboids
+    [labels, numClusters] = pcsegdist(cloud, 0.5);
+        
     % Draw point clouds
     figure(fig);
     pcshow(cloud.Location, labels);
     title(sprintf('Point Cloud Clusters @ %i',scenario.SimulationTime));
+    
+    % Mark vehicle location
+    plot(cuboidModel([1,0,1,1,1,0.5,0,0,0]));
      
     closest = [];
 
@@ -33,8 +44,6 @@ function [] = process(scenario, objects, ptCloud, world, vehicle)
         model = pcfitcuboid(cloud, idx);
         if prod(model.Dimensions) > 5
             %plot(model)
-            axes(world);
-            plot(cuboid2Inertial(model, vehicle));
             % Narrow down to vehicles in specific area
             if model.Center(1) > 0 && model.Center(2) < -0.5 && model.Center(2) > -2
                 % If it's closer than closest, use it
@@ -49,12 +58,30 @@ function [] = process(scenario, objects, ptCloud, world, vehicle)
     end
     %plot3(closest.Center(1), closest.Center(2), closest.Center(3));
     if ~isempty(closest)
-        figure(fig)
-        plot(closest)
+        figure(fig);
+        plot(closest);
+        ws = cuboid2Inertial(closest, vehicle);
+        if isempty(detected)
+            detected = [ws.Center];
+            axes(world);
+            plot(ws);
+        else
+            rows = ismembertol(detected, ws.Center, 0.15, 'ByRows', true);
+            if ~rows
+                detected = [detected; ws.Center];
+                axes(world);
+                plot(ws);
+            else
+                disp(detected);
+                disp(rows);
+                disp(detected(rows, :));
+                detected(rows, :) = ws.Center;
+            end
+        end
     end
         
     % Update message
-    message = sprintf('Number of objects sampled in one time step: %f\n', length(objects));
+    message = sprintf('Number of objects sampled in one time step: %f\n', length(detected));
     textField.String = message;
 end
 
