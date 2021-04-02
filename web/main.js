f9615415badb31ee12909846cc73c5ae1631cd82
Gregory Kelly <git@gregk.ca>
2021-04-02 02:55:17 -0400

    Added web backend

--- web/main.js
+++ web/main.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mysql = require('mysql');
+const CONFIG = require('./dbcfg.json');
 const app = express();
 const port = 3000;
 
+var conn = mysql.createConnection(CONFIG);
+
+conn.connect();
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname+"/index.html");
+});
+
+app.get('/data', (req, res) => {
+    conn.query("select * from vehicles order by id desc", function(error, results, field) {
+        if(error) throw error;
+        res.json(results);
+    });
 });
 
 app.listen(port, () => {
