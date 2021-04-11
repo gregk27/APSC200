5cca7c0db023601c9ea3d70d99035106f81d1e44
Gregory Kelly <git@gregk.ca>
2021-04-11 00:22:36 -0400

    Updated web interface for new database

--- web/main.js
+++ web/main.js
@@ -12,8 +12,20 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname+"/index.html");
 });
 
-app.get('/data', (req, res) => {
+app.get('/data/vehicles', (req, res) => {
     conn.query("select * from vehicles order by id desc", function(error, results, field) {
+        if(error) throw error;
+        res.json(results);
+    });
+});
+app.get('/data/areas/:scenario', (req, res) => {
+    conn.query(`select name,x0,y0,x1,y1 from areas where scenario='${req.params.scenario}'`, function(error, results, field) {
+        if(error) throw error;
+        res.json(results);
+    });
+});
+app.get('/data/exempt', (req, res) => {
+    conn.query(`select plate from exemptions`, function(error, results, field) {
         if(error) throw error;
         res.json(results);
     });
